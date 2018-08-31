85a9d5142bd5aa9761059681e68ac18613c7f595
Konstantin Harutyunyan <harutyunyankostya@yahoo.com>
2018-08-31 20:01:28 +0400

    Removed Fetch list by node and by user actions

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -4,12 +4,6 @@ import axios from 'axios'
 export const FETCH_LIST = 'orders/FETCH_LIST'
 export const FETCH_LIST_ERROR = 'orders/FETCH_LIST_ERROR'
 export const FETCH_LIST_SUCCESS = 'orders/FETCH_LIST_SUCCESS'
-export const FETCH_LIST_BY_NODE = 'orders/FETCH_LIST_BY_NODE'
-export const FETCH_LIST_BY_NODE_ERROR = 'orders/FETCH_LIST_BY_NODE_ERROR'
-export const FETCH_LIST_BY_NODE_SUCCESS = 'orders/FETCH_LIST_BY_NODE_SUCCESS'
-export const FETCH_LIST_BY_USER = 'orders/FETCH_LIST_BY_USER'
-export const FETCH_LIST_BY_USER_ERROR = 'orders/FETCH_LIST_BY_USER_ERROR'
-export const FETCH_LIST_BY_USER_SUCCESS = 'orders/FETCH_LIST_BY_USER_SUCCESS'
 
 // INITIAL STATE ///////////////////////////////////////////////////////////////
 const initialState = {
@@ -23,8 +17,6 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_LIST:
-    case FETCH_LIST_BY_NODE:
-    case FETCH_LIST_BY_USER:
       return {
         ...state,
         pending: true,
@@ -33,8 +25,6 @@ export default (state = initialState, action) => {
       }
 
     case FETCH_LIST_ERROR:
-    case FETCH_LIST_BY_NODE_ERROR:
-    case FETCH_LIST_BY_USER_ERROR:
       return {
         ...state,
         pending: false,
@@ -44,8 +34,6 @@ export default (state = initialState, action) => {
 
 
     case FETCH_LIST_SUCCESS:
-    case FETCH_LIST_BY_NODE_SUCCESS:
-    case FETCH_LIST_BY_USER_SUCCESS:
       return {
         ...state,
         list: action.payload,
@@ -69,36 +57,6 @@ export function fetchOrders() {
       })
       .catch((error) => {
         dispatch({ type: FETCH_LIST_ERROR, payload: {message: error.data} })
-        console.log(error)
-      })
-  }
-}
-
-export function fetchOrdersByNode(slug) {
-  return dispatch => {
-    dispatch({ type: FETCH_LIST_BY_NODE })
-    axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('jwt-nodebuckshq')
-    axios.get(`/api/orders/n=${slug}`)
-      .then((response) => {
-        dispatch({ type: FETCH_LIST_BY_NODE_SUCCESS, payload: response.data })
-      })
-      .catch((error) => {
-        dispatch({ type: FETCH_LIST_BY_NODE_ERROR, payload: {message: error.data} })
-        console.log(error)
-      })
-  }
-}
-
-export function fetchOrdersByUser(slug) {
-  return dispatch => {
-    dispatch({ type: FETCH_LIST_BY_USER })
-    axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('jwt-nodebuckshq')
-    axios.get(`/api/orders/u=${slug}`)
-      .then((response) => {
-        dispatch({ type: FETCH_LIST_BY_USER_SUCCESS, payload: response.data })
-      })
-      .catch((error) => {
-        dispatch({ type: FETCH_LIST_BY_NODE_ERROR, payload: {message: error.data} })
         console.log(error)
       })
   }
