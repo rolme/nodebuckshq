8db43497ef3a272ebcc51a07012d41af33c30c0c
Aleksandar M <salac@kolosek.com>
2018-09-25 10:58:09 +0200

    Added 2FA if enabled for loging user

---
+++ src/components/2fa.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input } from 'reactstrap';
+import speakeasy from 'speakeasy'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { login } from '../reducers/user'
+
+class Modal2FA extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      token: '',
+      message: '',
+    }
+  }
+
+  handleTokenChange = (e) => {
+    this.setState({ token: e.target.value })
+  }
+
+  handleSubmit = () => {
+    const { email, password, secret } = this.props
+    const isTokenValid = speakeasy.totp.verify({ 
+      secret: secret,
+      encoding: 'ascii',
+      token: this.state.token,
+    })
+
+    if(isTokenValid) {
+      this.props.login(email, password)
+      this.props.onToggle()
+    } else
+      this.setState({ message: 'Token is invalid'})
+  }
+
+  render() {
+    return (
+      <Modal isOpen={this.props.show} toggle={this.props.onToggle} className={this.props.className}>
+        <ModalHeader toggle={this.props.onToggle}>Two Factor Authentication</ModalHeader>
+        <ModalBody>
+          <Input 
+            type="password"
+            name="2fa"
+            placeholder="Enter google authenticator token here"
+            value={this.state.token}
+            onChange={this.handleTokenChange}
+          />
+          <p className="text-danger">{this.state.message}</p>
+        </ModalBody>
+        <ModalFooter>
+          <Button 
+            color="primary" 
+            onClick={this.handleSubmit}
+          >
+            Submit
+          </Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  error: state.user.error,
+  user: state.user.data,
+  message: state.user.message,
+  authenticated: state.user.data !== null
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  login,
+}, dispatch)
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Modal2FA)
+
