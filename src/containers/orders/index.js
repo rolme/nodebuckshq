babd2967802446a4d3f2067d11fb144a856c2edd
Aleksandar M <salac@kolosek.com>
2018-10-01 15:31:40 +0200

    Added paging and filtering for orders

--- src/containers/orders/index.js
+++ src/containers/orders/index.js
@@ -4,13 +4,27 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { Table } from 'reactstrap'
 import { valueFormat } from '../../lib/helpers'
-
-import {
-  fetchOrders,
-} from '../../reducers/orders'
-
+import { filter } from 'lodash'
+import Pagination from "react-js-pagination";
+import qs from 'query-string'
+import Dropdown from 'react-dropdown'
+import { fetchOrders } from '../../reducers/orders'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import { faAngleDown, faAngleUp } from '@fortawesome/fontawesome-free-solid'
+
+import 'react-dropdown/style.css'
+
+const TYPE_OPTIONS = [
+  { value: 'none', label: 'None' },
+  { value: 'buy', label: 'Buy' },
+  { value: 'sold', label: 'Sold' }
+]
+
+const STATUS_OPTIONS = [
+  { value: 'none', label: 'None' },
+  { value: 'paid', label: 'Paid' },
+  { value: 'unpaid', label: 'Unpaid' }
+]
 
 class Orders extends Component {
   constructor(props) {
@@ -18,6 +32,11 @@ class Orders extends Component {
     this.state = {
       sortedColumnName: 'node.createdAt',
       isDescending: false,
+      page: parseInt(qs.parse(props.location.search).page, 10) || 1,
+      limit: parseInt(qs.parse(props.location.search).limit, 10) || 25,
+      filterType: 'none',
+      filterStatus: 'unpaid',
+      sortedData: [],
     }
     this.onSortClick = this.onSortClick.bind(this)
   }
@@ -53,9 +72,18 @@ class Orders extends Component {
     return sortedData
   }
 
+  handlePageChange = (page) => {
+    this.setState({ page })
+    this.props.fetchOrders(page, this.state.limit)
+  }
+
+  onFilterSelect = (type) => (e) => {
+    this.setState({ [type]: e.value })
+  }
+
   render() {
-    let { list } = this.props
-    const { sortedColumnName, isDescending } = this.state
+    let { list, counts } = this.props
+    const { sortedColumnName, isDescending, filterType, filterStatus } = this.state
 
     if ( list === null ) {
       return <div>Loading Orders</div>
@@ -63,10 +91,30 @@ class Orders extends Component {
 
     list = this.sortTable(list)
 
+    if(filterType !== 'none') list = filter(list, (o) => { return o.orderType.toLowerCase() === filterType.toLowerCase() });
+    if(filterStatus !== 'none') list = filter(list, (o) => { return o.status.toLowerCase() === filterStatus.toLowerCase() });
+
     return (
       <div className="row">
         <div className="col-12 px-5">
           <h2 className="mt-2">Orders ({list.length})</h2>
+          <div>
+            <div className="d-flex justify-content-around align-items-center">
+              <Pagination
+                activePage={this.state.page}
+                itemsCountPerPage={this.state.limit}
+                totalItemsCount={counts.ordersAll}
+                pageRangeDisplayed={10}
+                onChange={this.handlePageChange}
+              />
+              <div className="d-flex align-items-center">
+                <div className="mr-1">Type:</div>
+                <Dropdown options={TYPE_OPTIONS} value={filterType} onChange={this.onFilterSelect('filterType')} />
+                <div className="ml-3 mr-1">Status:</div>
+                <Dropdown options={STATUS_OPTIONS} value={filterStatus} onChange={this.onFilterSelect('filterStatus')} />
+              </div>
+            </div>
+          </div>
           <Table striped hover responsive>
             <thead>
             <tr>
@@ -109,7 +157,8 @@ class Orders extends Component {
 }
 
 const mapStateToProps = state => ({
-  list: state.orders.list
+  list: state.orders.list,
+  counts: state.counts.data,
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
