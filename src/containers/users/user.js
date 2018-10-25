36a2795008226d3c0b61ac35f556c551da463251
Aleksandar M <salac@kolosek.com>
2018-10-25 16:47:15 +0200

    Added autosugest fields for affiliate dropdown

--- src/containers/users/user.js
+++ src/containers/users/user.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, Input } from 'reactstrap';
+import Autosuggest from 'react-autosuggest';
 
 import {
-  fetchUser
+  fetchUser,
+  fetchUsers,
 } from '../../reducers/users'
 
 import {
@@ -27,19 +30,73 @@ class User extends Component {
   constructor(props) {
     super(props)
     this.toggleAlert = this.toggleAlert.bind(this)
+    this.state = {
+      suggestions: [],
+      value: '',
+      value2: '',
+      value3: '',
+    }
   }
 
   componentWillMount() {
-    let { match: { params }, nodes, orders, withdrawals } = this.props
+    let { match: { params }, nodes, orders, withdrawals, list, user } = this.props
     this.props.fetchUser(params.slug)
     !nodes.length && this.props.fetchNodes()
     !orders.length && this.props.fetchOrders(1, Number.MAX_SAFE_INTEGER)
     !withdrawals.length && this.props.fetchWithdrawals()
+    if (list.length <= 1 && user !== '') {
+      this.props.fetchUsers()
+    }
   }
 
   toggleAlert() {
     this.props.clearMessages()
   }
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: this.getSuggestions(value)
+    });
+  };
+
+  getSuggestions = value => {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+
+    return inputLength === 0 ? [] : this.props.list.filter(user =>
+      user.email.toLowerCase().slice(0, inputLength) === inputValue && user.slug !== this.props.user.slug
+    );
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  getSuggestionValue = suggestion => suggestion.name;
+
+  renderSuggestion = suggestion => (
+    <div key={suggestion.slug}>{suggestion.email}</div>
+  );
+
+  onChangeTier1 = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+   onChangeTier2 = (event, { newValue }) => {
+    this.setState({
+      value2: newValue
+    });
+  };
+
+   onChangeTier3 = (event, { newValue }) => {
+    this.setState({
+      value3: newValue
+    });
+  };
 
   render() {
     const { match: { params }, user, nodes, orders, withdrawals, pending, error, message } = this.props
@@ -54,7 +111,7 @@ class User extends Component {
           <Alert color={error ? 'danger' : 'success'} isOpen={!!message} toggle={this.toggleAlert}>
             {message}
           </Alert>
-          <div className="col-10 offset-1 px-5">
+          <div className="col-10 offset-1 px-5 mb-4">
             <h2 className="mt-2">User</h2>
             <Table responsive striped>
               <thead>
@@ -69,6 +126,60 @@ class User extends Component {
               {this.renderUserData(user)}
               </tbody>
             </Table>
+            <UncontrolledDropdown>
+              <DropdownToggle caret>
+                Affiliates
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem header style={{ fontSize: 18 }}>
+                  <b>Tier1:</b> {user.affiliates.tier1 ? `${user.affiliates.tier1.first} ${user.affiliates.tier1.last} (${user.affiliates.tier1.email})` : ''}
+                </DropdownItem>
+                <Autosuggest
+                  suggestions={this.state.suggestions}
+                  onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                  onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                  getSuggestionValue={this.getSuggestionValue}
+                  renderSuggestion={this.renderSuggestion}
+                  inputProps={{
+                    placeholder: 'Type user email here',
+                    value: this.state.value || '',
+                    onChange: this.onChangeTier1
+                  }}
+                />
+                <DropdownItem divider/>
+                <DropdownItem header style={{ fontSize: 18 }}>
+                  <b>Tier2:</b> {user.affiliates.tier2 ? `${user.affiliates.tier2.first} ${user.affiliates.tier2.last} (${user.affiliates.tier2.email})` : ''}
+                </DropdownItem>
+                <Autosuggest
+                  suggestions={this.state.suggestions}
+                  onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                  onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                  getSuggestionValue={this.getSuggestionValue}
+                  renderSuggestion={this.renderSuggestion}
+                  inputProps={{
+                    placeholder: 'Type user email here',
+                    value: this.state.value2 || '',
+                    onChange: this.onChangeTier2
+                  }}
+                />
+                <DropdownItem divider/>
+                <DropdownItem header style={{ fontSize: 18 }}>
+                  <b>Tier3:</b> {user.affiliates.tier3 ? `${user.affiliates.tier3.first} ${user.affiliates.tier3.last} (${user.affiliates.tier3.email})` : ''}
+                </DropdownItem>
+                <Autosuggest
+                  suggestions={this.state.suggestions}
+                  onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                  onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                  getSuggestionValue={this.getSuggestionValue}
+                  renderSuggestion={this.renderSuggestion}
+                  inputProps={{
+                    placeholder: 'Type user email here',
+                    value: this.state.value3 || '',
+                    onChange: this.onChangeTier3
+                  }}
+                />
+              </DropdownMenu>
+            </UncontrolledDropdown>
           </div>
           <div className="col-10 offset-1 px-5">
             <h2 className="mt-2">Nodes</h2>
@@ -227,7 +338,8 @@ const mapStateToProps = state => ({
   withdrawals: state.withdrawals.list,
   error: state.users.error,
   message: state.users.message,
-  pending: state.users.pending
+  pending: state.users.pending,
+  list: state.users.list,
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
@@ -235,6 +347,7 @@ const mapDispatchToProps = dispatch => bindActionCreators({
   fetchNodes,
   fetchOrders,
   fetchWithdrawals,
+  fetchUsers,
 }, dispatch)
 
 export default connect(
